2806c1365cb0509e43518be476f2cb7976e0c65e
Vinay Verma <vvinay@codeaurora.org>
2018-07-19 14:34:27 +0530

    gem: init: Add /vendor/firmware_mnt/image to firmware_directory path
    uevent will read firmware_directory and add directory path
    to search list, firmware will be searched in this directory
    as well when serving firmware request.
    
    Change-Id: I14a27e0260ae182e574ac76010da26120f2d7d5f

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -48,7 +48,6 @@ BOARD_KERNEL_CMDLINE := \
     service_locator.enable=1 \
     lpm_levels.sleep_disabled=1 \
     androidboot.usbcontroller=a600000.dwc3 \
-    firmware_class.path=/vendor/firmware_mnt/image \
     loop.max_part=7
 BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
 BOARD_KERNEL_PAGESIZE := 4096
