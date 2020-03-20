f29cff9340e4537b1e9924ab3a5bb5c24d421758
Santhosh Behara <santhoshbehara@codeaurora.org>
2020-03-20 11:04:15 +0530

    gem: media: Add and publish Codec2 HAL service
    *Add 2 codec2 relates libs to PRODUCT_PACKAGES in order to copy
    them to /vendor/lib/
    *Add seccomp policies for codec2 services
    
    Change-Id: I56da6b774cf0f9b9e628886d7bfe22f5ccae4258

--- device.mk
+++ device.mk
@@ -192,6 +192,9 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/media/media_codecs_c2.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_c2.xml \
     $(LOCAL_PATH)/media/media_codecs_performance_c2.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_performance_c2.xml
 
+PRODUCT_COPY_FILES += \
+    hardware/qcom-caf/sm8150/media/conf_files/sm6150/codec2.vendor.ext.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/codec2.vendor.ext.policy
+
 # Shipping API level
 PRODUCT_SHIPPING_API_LEVEL := 28
 
