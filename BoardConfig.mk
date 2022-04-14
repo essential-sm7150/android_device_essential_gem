d567bd81eea20f4e55bfad481506cd6836c5b2fe
Alexander Koskovich <akoskovich@pm.me>
2022-04-13 17:01:15 -0700

    gem: Unset TARGET_CPU_ABI2
    Change-Id: I80230829ece46b0cb929832b737b82903d53d906

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -9,7 +9,6 @@ DEVICE_PATH := device/essential/gem
 TARGET_ARCH := arm64
 TARGET_ARCH_VARIANT := armv8-a
 TARGET_CPU_ABI := arm64-v8a
-TARGET_CPU_ABI2 :=
 TARGET_CPU_VARIANT := generic
 TARGET_CPU_VARIANT_RUNTIME := cortex-a76
 
