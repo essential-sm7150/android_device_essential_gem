cc0a6eae47442846ebe30a83ba879db089d2bad5
Roberto Sartori <roberto.sartori.android@gmail.com>
2022-12-29 10:30:17 +0100

    gem: Switch from time_daemon to TimeKeep
      * time_daemon just refuses to work properly on mata.
        Sony's TimeKeep, instead, does the job flawlessly,
        so just ditch the former in favor of the latter.
    
    Signed-off-by: Roberto Sartori <roberto.sartori.android@gmail.com>
    Change-Id: I54f50ed3d49efc2834d478781b15e6ee428a3062

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -95,6 +95,7 @@ TARGET_USERIMAGES_USE_F2FS := true
 # Sepolicy
 include device/qcom/sepolicy_vndr/SEPolicy.mk
 include device/lineage/sepolicy/libperfmgr/sepolicy.mk
+include hardware/sony/timekeep/sepolicy/SEPolicy.mk
 
 # Verified Boot
 BOARD_AVB_ENABLE := true
