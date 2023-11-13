c54c5d0f83ff7698f078637ef4a764882c0adc6b
Bruno Martins <bgcngm@gmail.com>
2023-11-13 13:48:00 +0000

    gem: Move to QTI health AIDL service
    The health AIDL HAL service provides functionalities of charger,
    therefore system charger at /system/bin/charger is deprecated.
    On top of that, QTI health AIDL HAL service enables suspend by
    default, the equivalent of setting ro.charger.enable_suspend
    for legacy charger.
    
    Change-Id: I59c23e7974cea1174b0161f31a535fa3afa1e5c9

--- device.mk
+++ device.mk
@@ -114,6 +114,11 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.vulkan.level-0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.vulkan.level-0.xml \
     frameworks/native/data/etc/android.hardware.vulkan.version-1_0_3.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.vulkan.version-1_0_3.xml
 
+# Health
+PRODUCT_PACKAGES += \
+    android.hardware.health-service.qti \
+    android.hardware.health-service.qti_recovery
+
 # Init
 PRODUCT_PACKAGES += \
     fstab.qcom \
