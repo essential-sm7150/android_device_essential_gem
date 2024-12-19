53d383b8a35787d111af9f94d1b2a4929fbb9eff
Michael Bestas <mkbestas@lineageos.org>
2024-12-19 22:01:32 +0200

    gem: gps: Remove usage of std::shared_ptr<T>::unique()
    It has been deprecated in c++17 and removed in c++20.
    Fixes:
    gps/utils/LocIpc.h:119:43: error: no member named 'unique' in
        'std::shared_ptr<int>'
      119 |         if (nullptr != mSocket && mSocket.unique()) {
          |                                   ~~~~~~~ ^
    1 error generated.
    
    Change-Id: I10b2be9d07ede68c4083e6a12f2bd5679f603402

--- gps/utils/LocIpc.h
+++ gps/utils/LocIpc.h
@@ -118,7 +118,7 @@ public:
     }
 
     inline ~LocIpcSender() {
-        if (nullptr != mSocket && mSocket.unique()) {
+        if (nullptr != mSocket && mSocket.use_count() == 1) {
             ::close(*mSocket);
         }
     }
