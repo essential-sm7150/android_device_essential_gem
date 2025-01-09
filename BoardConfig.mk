0fe39a138c3e44f5ef9f5243ba9e5680d16ec7ae
LuK1337 <priv.luk@gmail.com>
2025-01-10 00:22:04 +0100

    gem: Include common device matrices
    Change-Id: I86d8e202a8e48b9856c58d649925fee891bb26e4

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -26,6 +26,11 @@ TARGET_NO_BOOTLOADER := true
 # Display
 TARGET_SCREEN_DENSITY := 280
 
+# HIDL
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
+    hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml
+DEVICE_MATRIX_FILE := hardware/qcom-caf/common/compatibility_matrix.xml
+
 # Kernel
 BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_CMDLINE := \
