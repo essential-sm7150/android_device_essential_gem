70b6fd2c4a6103a870f6db8ef6e4d0da1865b8f7
LuK1337 <priv.luk@gmail.com>
2025-01-11 17:05:20 +0100

    gem: Create vendor mount points
    Change-Id: I13dc0d47145d1ca6dc1fdceb091fcc7c83cb8637

--- device.mk
+++ device.mk
@@ -59,6 +59,12 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/com.android.nfc_extras.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.android.nfc_extras.xml \
     frameworks/native/data/etc/com.nxp.mifare.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.nxp.mifare.xml
 
+# Partitions
+PRODUCT_PACKAGES += \
+    vendor_bt_firmware_mountpoint \
+    vendor_dsp_mountpoint \
+    vendor_firmware_mnt_mountpoint
+
 # Shipping API level
 PRODUCT_SHIPPING_API_LEVEL := 28
 
