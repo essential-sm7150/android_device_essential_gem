88fd637a480ad1d7de01439e9dc0f4f934dc7c3d
LuK1337 <priv.luk@gmail.com>
2025-01-17 10:24:46 +0100

    gem: Build clearkey DRM HAL
    Change-Id: Id35f3fbb4a9f727f6a0bad3f7bd6535cfd050f34

--- device.mk
+++ device.mk
@@ -102,6 +102,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     disable_configstore
 
+# DRM
+PRODUCT_PACKAGES += \
+    android.hardware.drm-service.clearkey
+
 # GNSS
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.location.gps.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.location.gps.xml
