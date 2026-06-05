b91f6677641a390153b44149ad00bfb07aec3763
Giovanni Ricca <giovanniricca@proton.me>
2026-06-05 15:37:20 +0200

    gem: Add arch, bootloader & platform flags
    Change-Id: I4c95f2accccdeb33bde05e36bc025cb7d33b6168

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -5,5 +5,26 @@
 
 DEVICE_PATH := device/essential/gem
 
+# Architecture
+TARGET_ARCH := arm64
+TARGET_ARCH_VARIANT := armv8-a
+TARGET_CPU_ABI := arm64-v8a
+TARGET_CPU_ABI2 :=
+TARGET_CPU_VARIANT := generic
+
+TARGET_2ND_ARCH := arm
+TARGET_2ND_ARCH_VARIANT := armv7-a-neon
+TARGET_2ND_CPU_ABI := armeabi-v7a
+TARGET_2ND_CPU_ABI2 := armeabi
+TARGET_2ND_CPU_VARIANT := cortex-a9
+
+# Bootloader
+TARGET_BOOTLOADER_BOARD_NAME := gem
+TARGET_NO_BOOTLOADER := true
+
+# Platform
+BOARD_USES_QCOM_HARDWARE := true
+TARGET_BOARD_PLATFORM := sm6150
+
 # Include the proprietary files BoardConfig.
 include vendor/essential/gem/BoardConfigVendor.mk
