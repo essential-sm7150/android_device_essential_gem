6278762db224be1e93f8c90ae5f842a7eb5128af
Giovanni Ricca <giovanniricca@proton.me>
2026-06-08 22:06:45 +0200

    [wip] gem: Initial proprietary files list
    - Kang display stack from NX627J to match oss display composer service requirements.
    - Kang DRM Widevine stack from NX627J to allow protected playbacks w/ L3.
    - Kang Peripheral Manager, VPP stacks (and their deps) from NX627J to unbreak them.
    - Kang C2 stack from mojito, is known to work on sdmmagpie / atoll targets.
    - a630_gmu.bin -> a618_gmu.bin to match with I7d0ba17aa16341c2a79d774efefaab1f415d64d7.
    
    To add: GPS, Secure Element
    
    Change-Id: I3257adca85426eae29faa3d3115ba6d8a6581e18

--- extract-files.py
+++ extract-files.py
@@ -4,6 +4,14 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+from extract_utils.fixups_blob import (
+    blob_fixup,
+    blob_fixups_user_type,
+)
+from extract_utils.fixups_lib import (
+    lib_fixups,
+    lib_fixups_user_type,
+)
 from extract_utils.main import (
     ExtractUtils,
     ExtractUtilsModule,
@@ -11,11 +19,62 @@ from extract_utils.main import (
 
 namespace_imports = [
     'device/essential/gem',
+    'hardware/qcom-caf/sm8150',
+    'hardware/qcom-caf/wlan',
+    'vendor/qcom/opensource/commonsys/display',
+    'vendor/qcom/opensource/commonsys-intf/display',
+    'vendor/qcom/opensource/dataservices',
+    'vendor/qcom/opensource/display',
 ]
+
+blob_fixups: blob_fixups_user_type = {
+    'system_ext/etc/permissions/qcrilhook.xml': blob_fixup()
+        .regex_replace('/system/', '/system_ext/'),
+    (
+        'vendor/bin/cnd',
+        'vendor/bin/imsdatadaemon',
+        'vendor/lib64/libril-qc-hal-qmi.so',
+    ): blob_fixup()
+        .add_needed('libhidlbase-v32.so'),
+    (
+        'vendor/bin/vppservice',
+        'vendor/lib/libvppclient.so',
+    ): blob_fixup()
+        .remove_needed('libhwbinder.so'),
+    'vendor/lib/hw/audio.primary.gem.so': blob_fixup()
+        .replace_needed('libaudioroute.so', 'libaudioroute-v34.so'),
+    'vendor/lib64/hw/camera.qcom.so': blob_fixup()
+        .add_needed('libcomparetf2_shim.so'),
+    'vendor/lib64/libdpps.so': blob_fixup()
+        .replace_needed('libtinyxml2.so', 'libtinyxml2-v34.so'),
+    'vendor/lib64/liboemcrypto.so': blob_fixup()
+        .remove_needed('libcommoncputils.so'),
+    'vendor/lib64/libvidhance.so': blob_fixup()
+        .add_needed('libcomparetf2_shim.so')
+        .add_needed('libdemangle.so'),
+    (
+        'vendor/lib64/libwvhidl.so',
+        'vendor/lib64/mediadrm/libwvdrmengine.so',
+    ): blob_fixup()
+        .add_needed('libcrypto_shim.so'),
+    ## TEMP ##
+    (
+        'vendor/lib64/com.quicinc.cne.api@1.0.so',
+        'vendor/lib64/com.quicinc.cne.api@1.1.so',
+        'vendor/lib64/com.quicinc.cne.server@2.0.so',
+        'vendor/lib64/com.quicinc.cne.server@2.1.so',
+        'vendor/lib64/com.quicinc.cne.server@2.2.so',
+        'vendor/lib64/com.quicinc.cne.server@2.3.so',
+        'vendor/lib64/vendor.qti.hardware.data.qmi@1.0.so',
+    ): blob_fixup()
+        .add_needed('libhidlbase_shim.so'),
+}  # fmt: skip
 
 module = ExtractUtilsModule(
     'gem',
     'essential',
+    blob_fixups=blob_fixups,
+    lib_fixups=lib_fixups,
     namespace_imports=namespace_imports,
 )
 
